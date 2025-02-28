29516d5f6d20a6fe3bb33af23e8596887e09f1db
akiritsa <akbcm98@gmail.com>
2025-02-28 14:48:09 +0300

    Create ExmSlice.go
    Added New Practise

---
+++ ExmSlice.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"error"
+)
+
+func main() {
+	var messages []string
+	messages[0] = "1"
+	11
+	fmt.Println(messages)
+	12
+	// messages := make([lstring)
+	14
+	// printMessages (messages)
+}
+
+func printMessages (messages [] string) error{
+		if len (messages) == 0{
+		return errors.New("empty array")
+	}
+		messages[0] = "5"
+		fmt.Println(messages)
+		return nil
+	}
